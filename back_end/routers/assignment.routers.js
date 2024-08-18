885ddc202c42bd5b2fed7c019089d9099c8db342
AndreP04 <andrepret4@icloud.com>
2024-08-18 12:14:13 +0200

    Added delete functionality for Assignments in Services, Controller and Router

--- back_end/routers/assignment.routers.js
+++ back_end/routers/assignment.routers.js
@@ -4,6 +4,9 @@ const AssignmentController = require('../controller/assignment.controller');
 //When the assign API is called
 router.post('/assignment', AssignmentController.assign);
 
+//When the delete API is called
+router.delete('/assignment', AssignmentController.delete);
+
 //router.post
 
 //Export the router so it accessible by the main application
