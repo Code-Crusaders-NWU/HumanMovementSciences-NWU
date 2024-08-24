6383caeec68519cb8fe1f2cf57dbe158497c587f
Stephan <143713572+Schmidt1230@users.noreply.github.com>
2024-08-24 18:12:29 +0200

    Added Sign-up documentation
    Created Swagger documentation for the signup API by providing descriptions and responses.
    
    Co-Authored-By: Johan-Louis Coetzee <143719943+JL-Coetzee@users.noreply.github.com>
    Co-Authored-By: Wimpie Botha <143725190+Wimpie-Botha@users.noreply.github.com>

--- back_end/routers/user.routers.js
+++ back_end/routers/user.routers.js
@@ -2,6 +2,68 @@
 const router = require('express').Router();
 const UserController = require("../controller/user.controller")
 
+/**
+ * @swagger
+ * /signup:
+ *   post:
+ *     summary: Register a new user
+ *     description: Registers new users with an email, password and user_type(admin, student, lecturer). There is various validation ensuring correct data is entered and there is no conflict by registering up an existing email.
+ *     tags: [User]
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             required:
+ *               - email
+ *               - password
+ *               - user_type
+ *             properties:
+ *               email:
+ *                 type: string
+ *                 format: email
+ *                 description: The user's email adress which will be used in the system as an identifier.
+ *                 example: user@example.com
+ *               password:
+ *                 type: string
+ *                 format: password
+ *                 description: A secure password between 8 and 30 characters, is also hashed once it is passed to the backend.
+ *                 example: password123
+ *               user_type:
+ *                 type: string
+ *                 enum: [student, lecturer, admin]
+ *                 description: Role of the user which is primarily used for access control, the lecturers can only be created by an admin and regular users are automatically created as students.
+ *                 example: student
+ *     responses:
+ *       201:
+ *         description: User registered successfully
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 status:
+ *                   type: string
+ *                   example: true
+ *                 success:
+ *                   type: string
+ *                   example: User has successfully signed up
+ *       500:
+ *         description: Internal Server Error - An error occurred during registration
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 success:
+ *                   type: boolean
+ *                   example: false
+ *                 message:
+ *                   type: string
+ *                   example: An error occurred during registration
+ */
+
 //When the signup api is called the register function is called from the user.coltroller.js file
 router.post('/signup',UserController.register);  
 
--- back_end/swagger.js
+++ back_end/swagger.js
@@ -5,13 +5,13 @@ openapi: '3.0.0',
 info: {
 title: 'HMS API',
 version: '1.0.0',
-description: 'API Documentation for performing CRUP RestAPI operations on HMS MongoDB database and interacting with the backend.',
+description: 'API Documentation for performing CRUD RestAPI operations on HMS MongoDB database and interacting with the backend.',
 },
 };
 
 const options = {
 swaggerDefinition,
-apis: ['./routes/*.js'], // Path to the API routes in your Node.js application
+apis: ['./routers/*.js'], // Path to the API routes in your Node.js application
 };
 
 const swaggerSpec = swaggerJSDoc(options);
