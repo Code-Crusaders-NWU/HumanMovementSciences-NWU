e7fbe9f1c261373ab36a0b498aec755bf90c7c9a
Stephan <143713572+Schmidt1230@users.noreply.github.com>
2024-08-24 17:43:36 +0200

    Added Swagger API Docs
    We have added Swagger API documentation to the NodeJS to allow us to streamline the documentation process and create easy API calls to our Backend using Swagger.
    
    Co-Authored-By: Johan-Louis Coetzee <143719943+JL-Coetzee@users.noreply.github.com>
    Co-Authored-By: Wimpie Botha <143725190+Wimpie-Botha@users.noreply.github.com>

--- back_end/app.js
+++ back_end/app.js
@@ -1,15 +1,24 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const userRouter = require('./routers/user.routers');
-const assignment_router = require('./routers/assignment.routers')
-const video_router = require('./routers/video.routers')
+const assignment_router = require('./routers/assignment.routers');
+const video_router = require('./routers/video.routers');
 const student_router = require('./routers/student.router');
+
+// Swagger
+const swaggerUI = require('swagger-ui-express');
+const swaggerSpec = require('./swagger');
+
 const app = express();
 
 app.use(bodyParser.json());
 
+// Serve Swagger documentation
+app.use('/api-docs', swaggerUI.serve, swaggerUI.setup(swaggerSpec));
+
 app.use('/', userRouter);
 app.use('/', assignment_router);
 app.use('/', video_router);
-app.use('/', student_router)
+app.use('/', student_router);
+
 module.exports = app;
