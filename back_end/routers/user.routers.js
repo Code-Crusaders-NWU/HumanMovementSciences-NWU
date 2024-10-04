9579d3cb22455deee0406ef5db2796b42ca8f87d
AndreP04 <andrepret4@icloud.com>
2024-10-04 12:39:25 +0200

    Added Swagger documentation for searchUser and getAllUsers

--- back_end/routers/user.routers.js
+++ back_end/routers/user.routers.js
@@ -182,13 +182,198 @@ router.post('/login',UserController.login);
  *                   type: string
  *                   example: "An error has occurred during user deletion"
  */
+
 //When the Delete API is called
 router.delete('/user',authenticateToken, isAdmin.isAdmin ,UserController.delete);    //Only admin users can delete accounts
 
 //When the getAllUsers API is called
 router.get('/allUsers', authenticateToken, isAdmin.isAdmin, UserController.getAllUsers)
 
+/**
+ * @swagger
+ * /api/allUsers:
+ *   get:
+ *     summary: Retrieve a list of all users
+ *     tags: 
+ *       - User
+ *     description: This endpoint retrieves all users from the database. It requires authentication and admin privileges.
+ *     security:
+ *       - bearerAuth: []
+ *     responses:
+ *       200:
+ *         description: A list of users was successfully retrieved.
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 status:
+ *                   type: boolean
+ *                   example: true
+ *                 users:
+ *                   type: array
+ *                   items:
+ *                     type: object
+ *                     properties:
+ *                       _id:
+ *                         type: string
+ *                         description: User ID.
+ *                         example: "61616f3e2f36c5b1a45b1234"
+ *                       email:
+ *                         type: string
+ *                         description: Email address of the user.
+ *                         example: "admin@example.com"
+ *                       user_type:
+ *                         type: string
+ *                         description: Role of the user.
+ *                         example: "admin"
+ *       401:
+ *         description: Unauthorized access.
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 success:
+ *                   type: boolean
+ *                   example: false
+ *                 message:
+ *                   type: string
+ *                   example: "Unauthorized access"
+ *       403:
+ *         description: Forbidden. Admin privileges required.
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 success:
+ *                   type: boolean
+ *                   example: false
+ *                 message:
+ *                   type: string
+ *                   example: "User must be an admin."
+ *       500:
+ *         description: Internal server error.
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 success:
+ *                   type: boolean
+ *                   example: false
+ *                 message:
+ *                   type: string
+ *                   example: "Failed to get users"
+ *                 error:
+ *                   type: string
+ *                   example: "Internal Server Error"
+ */
+
+
 //When the searchUser API is called
 router.get('/searchUser', authenticateToken, isAdmin.isAdmin, UserController.searchUser)
+
+/**
+ * @swagger
+ * /api/searchUser:
+ *   get:
+ *     summary: Search for a user by their email address.
+ *     tags: 
+ *       - User
+ *     description: This endpoint allows you to search for a user by their email address. It requires authentication and admin privileges.
+ *     security:
+ *       - bearerAuth: []
+ *     parameters:
+ *       - in: query
+ *         name: email
+ *         schema:
+ *           type: string
+ *         required: true
+ *         description: The email address of the user to search for.
+ *         example: "user@example.com"
+ *     responses:
+ *       200:
+ *         description: A list of users matching the search criteria was successfully retrieved.
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 status:
+ *                   type: boolean
+ *                   example: true
+ *                 users:
+ *                   type: array
+ *                   items:
+ *                     type: object
+ *                     properties:
+ *                       _id:
+ *                         type: string
+ *                         description: User ID.
+ *                         example: "61616f3e2f36c5b1a45b1234"
+ *                       email:
+ *                         type: string
+ *                         description: Email address of the user.
+ *                         example: "user@example.com"
+ *                       user_type:
+ *                         type: string
+ *                         description: Role of the user.
+ *                         example: "admin"
+ *       400:
+ *         description: Invalid or missing email address.
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 success:
+ *                   type: boolean
+ *                   example: false
+ *                 message:
+ *                   type: string
+ *                   example: "Invalid or missing email address"
+ *       401:
+ *         description: Unauthorized access.
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 success:
+ *                   type: boolean
+ *                   example: false
+ *                 message:
+ *                   type: string
+ *                   example: "Unauthorized access"
+ *       403:
+ *         description: Forbidden. Admin privileges required.
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 success:
+ *                   type: boolean
+ *                   example: false
+ *                 message:
+ *                   type: string
+ *                   example: "User must be an admin."
+ *       500:
+ *         description: Internal server error.
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 status:
+ *                   type: boolean
+ *                   example: false
+ *                 message:
+ *                   type: string
+ *                   example: "Internal Server Error"
+ */
+
 
 module.exports = router; //Export router so it can be used by the main application file
