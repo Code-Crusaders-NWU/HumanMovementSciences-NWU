da6e2b37d5cf17db29c725b238b9bc58d41218c7
JL-Coetzee <143719943+JL-Coetzee@users.noreply.github.com>
2024-10-07 18:16:44 +0200

    Changed S3service.js to return links to files in bucket

--- back_end/Storage/S3service.js
+++ back_end/Storage/S3service.js
@@ -1,20 +1,33 @@
 const {S3} = require("aws-sdk");                              //imported aws-sdk
 const uuid = require("uuid").v4;                              //imported uui for name handeling
 
+const s3 = new S3()
+
 
 exports.s3UploadV2 = async(files) => {             
-    const s3 = new S3();                                     //initialize s3 const
-
+    
     //params is an array of all different files
     const params = files.map(file => {
         return {
          Bucket: process.env.AWS_BUCKET_NAME,                //gets info from env file
          Key: `Uploads/${uuid()}-${file.originalname}`,      //name of file to be uploaded
          Body: file.buffer,
+         ACL: 'public-read'
         };
-    })
+    });
 
-    return await Promise.all(params.map(param => s3.upload(param).promise()));
+    const upRes = await promise.all(
+        params.map(param => s3.upload(param).promise())
+    );
+
+    //return URL to uploaded object
+    const fileLinks = upRes.map(result =>{
+        return `https://${result.Bucket}.s3.${process.env.AWS_REGION}.amazonaws.com/${result.Key}`
+    });
+    
+
+
+    return fileLinks;
 
 
 };
